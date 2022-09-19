c3a8148104c61a07c5033365def78aac4919dc53
dalcek <ivanak90@gmail.com>
2022-09-19 12:19:50 -0700

    Added functionalities

---
+++ projects/beta/src/app/interceptor/interceptor.ts
@@ -0,0 +1,21 @@
+import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+
+    var authToken = "";
+    // Clone the request and replace the original headers with
+    // cloned headers, updated with the authorization.
+    const authReq = req.clone({
+      headers: req.headers.set('Authorization', authToken)
+    });
+
+    // send cloned request with header to the next handler.
+    return next.handle(authReq);
+  }
+}
